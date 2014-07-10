0f182da5cb2c96fec65d2008a93fe35d87472851
Anton Ekblad <anton@ekblad.cc>
2014-07-10 20:26:33 +0200

    Haddock Scheduler.

--- Network/LambNyaa/Scheduler.hs
+++ Network/LambNyaa/Scheduler.hs
@@ -5,6 +5,7 @@ import Control.Monad (forM_)
 import Network.LambNyaa.Config
 import Network.LambNyaa.Types
 
+-- | Pause thread for a number of seconds.
 delaySecs :: Int -> IO ()
 delaySecs n = do
     forM_ [1..ksecs] . const $ threadDelay (1000 * 1000000)
@@ -12,6 +13,7 @@ delaySecs n = do
   where
     (ksecs, secs) = n `divMod` 1000
 
+-- | Execute a pipeline according to schedule.
 schedule :: Config -> IO ()
 schedule cfg = do
   case cfgSchedule cfg of
@@ -21,9 +23,11 @@ schedule cfg = do
     (Every n Hours)   -> every (n*60*60) $ execute cfg
     (Every n Days)    -> every (n*24*60*60) $ execute cfg
 
+-- | Perform an action every n seconds.
 every :: Int -> IO () -> IO ()
 every secs m = go where go = m >> delaySecs secs >> go
 
+-- | Execute a pipeline, from Source to Sink.
 execute :: Config -> IO ()
 execute cfg = do
   itemses <- mapM unSource $ cfgSources cfg
